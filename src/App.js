fd794eeae80ab74a87a9e6f274da31baab9c00f6
Kato <kubkate@gmail.com>
2020-11-16 13:21:14 -0800

    display current song

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { useState } from "react"
+
 import "./styles/app.scss"
+import data from "./data"
 
 import Player from './components/Player';
 import Song from './components/Song';
 
 function App() {
+
+  // get whole array of object form calling data function
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div>
-      <Song />
+      <Song currentSong={currentSong} />
       <Player />
     </div>
   );
